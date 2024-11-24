36428dd55fc2a1769addbcbea787902fdd18ab3e
Kevin London <kevinlondon@gmail.com>
2024-11-23 22:18:51 -0800

    The end of the tutorial!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://astro-tutorial-kl.netlify.app"
+  site: "https://astro-tutorial-kl.netlify.app",
+  integrations: [preact()]
 });
